a914593a5d2f224d93ab42f9b8ac528f882d4fc9
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-26 14:53:21 +0300

    ExplosionCube.cs

--- ExplosionCube.cs
+++ ExplosionCube.cs
@@ -1,18 +1,21 @@
-﻿using System;
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody))]
 public class ExplosionCube : MonoBehaviour
 {
-    public int Generation { get; private set; } = 1;
+    public event Action<EventParameters> HasExploded;
 
-    public event Action<Vector3, Vector3, int> OnExplode;
+    public int Generation { get; private set; } = 1;
 
     public void Explode()
     {
         if (ShouldSplit())
-            OnExplode?.Invoke(transform.position, transform.localScale, Generation);
+        {
+            var parameters = new EventParameters(transform.position, transform.localScale, Generation);
+            HasExploded?.Invoke(parameters);
+        }           
 
         Destroy(gameObject);
     }
@@ -21,18 +24,5 @@ public class ExplosionCube : MonoBehaviour
 
     private bool ShouldSplit() => Random.value <= GetSplitChance();
 
-    private float GetSplitChance()
-    {
-        switch (Generation)
-        {
-            case 1:
-                return 1.0f;
-            case 2:
-                return 0.5f;
-            case 3:
-                return 0.25f;
-            default:
-                return 0.0f;
-        }
-    }
+    private float GetSplitChance() => 1.0f / (1 << (Generation - 1));
 }
