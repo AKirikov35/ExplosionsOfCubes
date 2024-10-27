ed73765209e07ee7d1cc9c622c70ee5726bd2aae
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-27 12:15:51 +0300

    EventParameters.cs

--- EventParameters.cs
+++ EventParameters.cs
@@ -4,12 +4,13 @@ public class EventParameters
 {
 	public Vector3 Position { get; private set; }
 	public Vector3 Scale { get; private set; }
-	public int Generation { get; private set; }
+	public float SplitChance { get; private set; }
 
-	public EventParameters(Vector3 position, Vector3 scale, int generation)
+
+    public EventParameters(Vector3 position, Vector3 scale, float splitChance)
 	{
 		Position = position;
 		Scale = scale;
-		Generation = generation;
+		SplitChance = splitChance;
 	}
 }
