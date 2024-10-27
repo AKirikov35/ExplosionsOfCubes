da8f16c19c2858f35826185e6b9a75176c036450
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-27 12:16:38 +0300

    ExplosionCube.cs

--- ExplosionCube.cs
+++ ExplosionCube.cs
@@ -7,22 +7,38 @@ public class ExplosionCube : MonoBehaviour
 {
     public event Action<EventParameters> HasExploded;
 
-    public int Generation { get; private set; } = 1;
+    private Renderer _renderer;
+    private float _splitChance = 1f;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void Init(float initialSplitChance)
+    {
+        _splitChance = initialSplitChance;
+    }
 
     public void Explode()
     {
         if (ShouldSplit())
         {
-            var parameters = new EventParameters(transform.position, transform.localScale, Generation);
+            var parameters = new EventParameters(transform.position, transform.localScale, _splitChance);
             HasExploded?.Invoke(parameters);
-        }           
+        }
 
         Destroy(gameObject);
     }
 
-    public void SetGeneration(int generation) => Generation = generation;
+    public void SetColor(Color color)
+    {
+        if (_renderer != null)
+            _renderer.material.color = color;
+    }
 
-    private bool ShouldSplit() => Random.value <= GetSplitChance();
-
-    private float GetSplitChance() => 1.0f / (1 << (Generation - 1));
+    private bool ShouldSplit()
+    {
+        return Random.value <= _splitChance;
+    }
 }
