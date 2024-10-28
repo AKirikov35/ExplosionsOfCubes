7aad78654de04f37ae7a221e7469fb6c8df9650b
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-28 14:08:11 +0300

    ExplosionCube.cs

--- ExplosionCube.cs
+++ ExplosionCube.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Renderer))]
 public class ExplosionCube : MonoBehaviour
 {
     public event Action<EventParameters> HasExploded;
+
+    private readonly float _baseDetonationRadius = 100f;
+    private readonly float _baseDetonationForce = 1000f;
+
+    private readonly float _minDetonationRadius = 30f;
+    private readonly float _minDetonationForce = 100f;
+
+    private readonly float _forceModifier = 100f;
 
     private Renderer _renderer;
     private float _splitChance = 1f;
@@ -27,6 +37,10 @@ public class ExplosionCube : MonoBehaviour
             var parameters = new EventParameters(transform.position, transform.localScale, _splitChance);
             HasExploded?.Invoke(parameters);
         }
+        else
+        {
+            Detonate();
+        }
 
         Destroy(gameObject);
     }
@@ -40,5 +54,50 @@ public class ExplosionCube : MonoBehaviour
     private bool ShouldSplit()
     {
         return Random.value <= _splitChance;
+    }
+
+    private void Detonate()
+    {
+        List<Rigidbody> detonateObjects = GetDetonateObjects(_baseDetonationRadius);
+
+        foreach (Rigidbody rigidbody in detonateObjects)
+        {
+            float distance = Vector3.Distance(transform.position, rigidbody.transform.position);
+            float size = GetObjectSize(rigidbody);
+
+            float radius = Mathf.Max(_baseDetonationRadius * (1 / size), _minDetonationRadius);
+            float force = Mathf.Max(_baseDetonationForce * (1 / size), _minDetonationForce);
+
+            force *= _forceModifier;
+
+            float effectiveForce = force * (1 - (distance / radius));
+            effectiveForce = Mathf.Max(effectiveForce, _minDetonationForce);
+
+            rigidbody.AddExplosionForce(effectiveForce, transform.position, radius);
+
+            Debug.Log($"Взрыв на позиции {transform.position.normalized}\n" +
+                      $"Радиус: {radius}, " +
+                      $"Сила: {force}");
+        }
+    }
+
+    private List<Rigidbody> GetDetonateObjects(float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius);
+        List<Rigidbody> explosionCubes = new();
+
+        foreach (Collider hit in hits)
+            if (hit.attachedRigidbody != null)
+                explosionCubes.Add(hit.attachedRigidbody);
+
+        return explosionCubes;
+    }
+
+    private float GetObjectSize(Rigidbody rigidbody)
+    {
+        if (rigidbody.TryGetComponent<Collider>(out var collider))
+            return collider.bounds.size.magnitude;
+
+        return 1f;
     }
 }
