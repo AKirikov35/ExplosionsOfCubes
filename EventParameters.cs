6d4d782dc67afe93cb937a599643f9ad6b94dd3e
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-10-29 12:27:01 +0300

    EventParameters.cs

--- EventParameters.cs
+++ EventParameters.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class EventParameters
 {
-	public Vector3 Position { get; private set; }
-	public Vector3 Scale { get; private set; }
-	public float SplitChance { get; private set; }
+    public Vector3 Position { get; private set; }
+    public Vector3 Scale { get; private set; }
+    public float SplitChance { get; private set; }
+    public ExplosionCube Cube { get; private set; }
 
-
-    public EventParameters(Vector3 position, Vector3 scale, float splitChance)
-	{
-		Position = position;
-		Scale = scale;
-		SplitChance = splitChance;
-	}
+    public EventParameters(Vector3 position, Vector3 scale, float splitChance, ExplosionCube cube)
+    {
+        Position = position;
+        Scale = scale;
+        SplitChance = splitChance;
+        Cube = cube;
+    }
 }
